2bf0fb3e8de5670f117ed67565739c771130a514
Elisa BELMAIN <elisa.belmain@gmail.com>
2023-06-20 08:17:42 +0200

    LOCAL_MODEL_PATH is now a relative path. IMG_SIZE no longer in .env

--- art_guessing/params.py
+++ art_guessing/params.py
@@ -7,9 +7,11 @@ GCP_PROJECT = os.environ.get("GCP_PROJECT")      # get GCP_PROJECT from .env (pe
 BUCKET_NAME = os.environ.get("BUCKET_NAME")      # get BUCKET_NAME from .env (cloud storage)
 ##################  CONSTANTS  #####################
 LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), "code", "lailadib", "art_guessing", "art_guessing", "raw_data", "artbench_10")
-LOCAL_MODEL_PATH =  os.path.join(os.path.expanduser('~'), "code", "lailadib", "art_guessing", "art_guessing", "models")
+# LOCAL_MODEL_PATH =  os.path.join(os.path.expanduser('~'), "code", "lailadib", "art_guessing", "art_guessing", "models")
+#Relative path
+LOCAL_MODEL_PATH = "./models"
 
-IMG_SIZE = os.environ.get("IMG_SIZE")
+IMG_SIZE = 256
 INPUT_SHAPE = (int(IMG_SIZE),int(IMG_SIZE), 3)
 CLASS_NAMES = ['art_nouveau',
     'baroque',
